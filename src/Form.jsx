cb70fc11d2e7a40b16c7f479a31b95e375139a6b
nceballosp <nataliaceballospos@gmail.com>
2024-09-12 21:15:47 -0500

    Inputs

--- src/Form.jsx
+++ src/Form.jsx
@@ -3,30 +3,68 @@ import { useState,useEffect } from 'react'
 import './Form.css'
 
 function Form() {
-useEffect(()=>{
-  const metodo = document.querySelector('#metodo');
-  console.log(metodo);
-})
 const [eleccion,setEleccion] = useState('Biseccion');
   return (
     
     <form action="" id='formulario'>
      <select name="metodo" id="metodo" onInput={()=>setEleccion((method)=> metodo.value)} >
         <option value="Biseccion">Biseccion</option>
-        <option value="Busquedas Incrementales">Busquedas Incrementales</option>
+        <option value="Regla Falsa">Regla Falsa</option>
+        <option value="Punto Fijo">Punto Fijo</option>
         <option value="Newton">Newton</option>
+        <option value="Raices Multiples">Raices Multiples</option>
       </select>
-      {/* Completar lo inputs con condicionales para mostarse en los metodos que aplican */}
-      {/* Agregar labels a cada input */}
+
+      <label htmlFor="func">Funcion</label>
       <input type="text" name="func" id="func" placeholder='Ingrese la funcion a evaluar'/>
-      {/* ejemplo de conditional rendering */}
-      {eleccion==='Biseccion' && 
+      
+      <label htmlFor="Tol">Tolerancia</label>
+      <input type="text" name='Tol' id='Tol' placeholder='Tol'/>
+
+      <label htmlFor="Niter">Numero de Iteraciones</label>
+      <input type="text" name='Niter' id='Niter' placeholder='Niter'/>
+
+      {(eleccion==='Biseccion' || eleccion === 'Regla Falsa') &&
       <>
-      <label htmlFor="X0">Inicio de Intervalo</label>
-      <input type="text" name='X0'placeholder='X0' />
+      <label htmlFor="Xi">Inicio de Intervalo</label>
+      <input type="text" name='Xi' id='Xi' placeholder='Xi' />
       </>
       }
-     
+
+      {(eleccion==='Biseccion' || eleccion === 'Regla Falsa') &&
+      <>
+      <label htmlFor="Xf">Fin de Intervalo</label>
+      <input type="text" name='Xf' id='Xf' placeholder='Xf' />
+      </>
+      }
+
+      {eleccion==='Punto Fijo' &&
+      <>
+      <label htmlFor="g">gdex</label>
+      <input type="text" name='g' id='g' placeholder='g(x)'/>
+      </>
+      }
+
+     {(eleccion==='Punto Fijo' || eleccion==='Newton') &&
+      <>
+      <label htmlFor="X0">X inicial</label>
+      <input type="text" name='X0' id='X0' placeholder='X0' />
+      </>
+      }
+      
+      {/* {(eleccion==='Newton' || eleccion==='Raices Multiples') &&
+      <>
+      <label htmlFor="df">Derivada f</label>
+      <input type="text" name='df' id='df' placeholder='df'/>
+      </>
+      } */}
+      
+      {/* {eleccion==='Raices Multiples' && 
+      <>
+      <label htmlFor="dff">Seguna Derivada f</label>
+      <input type="text" name='dff' id='dff' placeholder='dff'/>
+      </>
+      } */}
     </form>
     
   )

e7c5f29dda7774d37198bd0b1f2d6d0bfe4b121e
nceballosp <nataliaceballospos@gmail.com>
2024-09-23 23:40:31 -0500

    ErrorType Agregado

--- ServidorPython/PuntoFijo.py
+++ ServidorPython/PuntoFijo.py
@@ -1,41 +1,35 @@
 import pandas as pd
 import numpy as np
-from SimpleWriting import *
-#import wdb
-#wdb.set_trace()
+import math
 
-def secante(X0,X1,Tol,Niter,Fun,ErrorType):
+
+def PuntoFijo(Fun, Tol, Niter, X0, g, ErrorType):
     fn=[]
     xn=[]
     E=[]
     N=[]
     x=X0
     f=eval(Fun)
-    fn.append(f)
-    x=X1
-    f = eval(Fun)
-    fn.append(f)
     c=0
     Error=100               
-    xn.append(X0)
-    xn.append(X1)
+    fn.append(f)
+    xn.append(x)
     E.append(Error)
     N.append(c)
-
     while Error>Tol and f!=0 and c<Niter:
-        c=c+1
-        x = xn[c] - (fn[c]*(xn[c]-xn[c-1])/(fn[c]-fn[c-1]))
-        f=eval(Fun)
-        fn.append(f)
+        x=eval(g)
+        fe=eval(Fun)
+        fn.append(fe)
         xn.append(x)
+        c=c+1
         N.append(c)
         if ErrorType == 'Abs':
-                Error=abs(xn[c+1]-xn[c])
+                Error=abs(xn[c]-xn[c-1])
                 E.append(Error)
         elif ErrorType == 'Rel':
-            Error = abs((xn[c+1]-xn[c])/(xn[c+1]))
+            Error = abs((xn[c]-xn[c-1])/(xn[c]))
             E.append(Error)
-    if f==0:
+    if fe==0:
         s=x
         state = 'Exact'
         tabla = list(zip(N,xn,fn,E))
@@ -51,5 +45,4 @@ def secante(X0,X1,Tol,Niter,Fun,ErrorType):
         return {'state':state,'message':f'Fracaso en {Niter} iteraciones'}
 
 
-#if __name__ == '__main__':
-  
+
--- ServidorPython/ReglaFalsa.py
+++ ServidorPython/ReglaFalsa.py
@@ -1,8 +1,6 @@
 import pandas as pd
 import numpy as np
 from SimpleWriting import *
-#import wdb
-#wdb.set_trace()
 
 def ReglaFalsa(Xi,Xs,Tol,Niter,Fun,ErrorType):
     fm=[]
@@ -17,11 +15,15 @@ def ReglaFalsa(Xi,Xs,Tol,Niter,Fun,ErrorType):
     if fi==0:
         s=Xi
         E=0
-        print(Xi, "es raiz de f(x)")
+        state = 'Exact'
+        tabla = [(0,s,fi,E)]
+        return {'state':state,'tabla':tabla,'sol':s}
     elif fs==0:
         s=Xs
         E=0
-        print(Xs, "es raiz de f(x)")
+        state = 'Exact'
+        tabla = [(0,s,fs,E)]
+        return {'state':state,'tabla':tabla,'sol':s}
     elif fs*fi<0:
         c=0
         N.append(c)
@@ -46,8 +48,12 @@ def ReglaFalsa(Xi,Xs,Tol,Niter,Fun,ErrorType):
             x=Xm 
             fe=eval(Fun)
             fm.append(fe)
-            Error=abs(Xm-Xa)
-            E.append(Error)
+            if ErrorType == 'Abs':
+                Error=abs(Xm-Xa)
+                E.append(Error)
+            elif ErrorType == 'Rel':
+                Error = abs((Xm-Xa)/Xm)
+                E.append(Error)
             c=c+1
             N.append(c)
         if fe==0:
@@ -55,8 +61,6 @@ def ReglaFalsa(Xi,Xs,Tol,Niter,Fun,ErrorType):
             state='Exact'
             tabla = zip(N,xn,fm,E)
             return {'state':state,'sol':sol,'tabla':tabla}
-            # print(s,"es raiz de f(x)")
-            # Solucion exacta
         elif Error<Tol:
                 sol=x
                 state = 'Aprox'
@@ -67,12 +71,10 @@ def ReglaFalsa(Xi,Xs,Tol,Niter,Fun,ErrorType):
             s=x
             state='Failed'
             return {'state':state,'Niter':Niter}
-            print("Fracaso en ",Niter, " iteraciones ") 
     else:
         # Invalido
         state='Invalid'
         return state
-        print("El intervalo es inadecuado")
 
 if __name__ == '__main__':
     print(ReglaFalsa(1, 2, 0.0001, 100, "(x**3)+(4*(x**2))-10"))
